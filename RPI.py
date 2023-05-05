f996bfb34d366d5877d8eb0684b816346ab47e10
Isaiah Lee <iclee@usc.edu>
2023-05-05 15:47:04 -0700

    Update RPI.py

--- RPI.py
+++ RPI.py
@@ -23,15 +23,15 @@ def on_connect(client, userdata, flags, rc):
     
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    client.subscribe("iclee/ping")
+    client.subscribe("iclee/input")
 
-    client.message_callback_add("iclee/ping", on_message_from_pong)
+    client.message_callback_add("iclee/input", on_message_from_input)
 
 def on_message(client, userdata, msg):
 
     print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
 
-def on_message_from_pong(client, userdata, message):
+def on_message_from_input(client, userdata, message):
     
     message = message.payload.decode()
     print("Message: "+message)

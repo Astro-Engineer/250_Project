c645d3e02f771f8eb3fac55816ac90b9a8ea8fc3
Isaiah Lee <iclee@usc.edu>
2023-05-05 15:45:10 -0700

    Update VM.py

--- VM.py
+++ VM.py
@@ -12,19 +12,26 @@ def on_connect(client, userdata, flags, rc):
     
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    client.subscribe("iclee/pong")
+    client.subscribe("iclee/data")
 
-    client.message_callback_add("iclee/pong", on_message_from_pong)
+    client.message_callback_add("iclee/data", on_message_from_data)
+    
+    client.subscribe("iclee/wrong")
+    
+    client.message_callback_add("iclee/data", on_message_from_wrong)
 
 def on_message(client, userdata, msg):
 
     print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
 
-def on_message_from_pong(client, userdata, message):
+def on_message_from_data(client, userdata, message):
 
     data = json.loads(message.payload)
     for element in data:
         print(element)
+        
+def on_message_from_data(client, userdata, message):
+    print("Please enter either 'Light', 'Sound', or 'Distance'!")
 
 if __name__ == '__main__':
 

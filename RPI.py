4ff84c014cdb1654e05b2ba20d99be8c11a13e6c
Isaiah Lee <iclee@usc.edu>
2023-05-05 15:38:17 -0700

    Update RPI.py

--- RPI.py
+++ RPI.py
@@ -45,7 +45,7 @@ def on_message_from_pong(client, userdata, message):
         message = json.dumps(values)
             
         client.publish("iclee/pong", message)
-        print("pubbed1")
+        print("pubbed1" + str(len(values))
     elif(message.lower() == "sound"):
         value = mcp.read_adc(1)
         message = value

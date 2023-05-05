83db34864950bcc9216e0aaccf4c7f4c95d895ec
Isaiah Lee <iclee@usc.edu>
2023-05-05 14:15:07 -0700

    Update RPI_code.py

--- RPI_code.py
+++ RPI_code.py
@@ -8,10 +8,14 @@ import socket
 
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+import RPi.GPIO as GPIO
 
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
 
 def on_connect(client, userdata, flags, rc):
     

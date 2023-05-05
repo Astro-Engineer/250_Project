ab9d48320d6c74c58c254ec0c03c91b0f3c7b799
mruiz20 <ruizmich@usc.edu>
2023-05-05 13:03:25 -0700

    update plot

--- plotting.py
+++ plotting.py
@@ -5,4 +5,8 @@ xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
 plt.plot(xpoints, ypoints)
+plt.xlabel("X-Label")
+plt.ylabel("Y-Label")
+plt.title("Title")
+plt.savefig("Sample")
 plt.show()
